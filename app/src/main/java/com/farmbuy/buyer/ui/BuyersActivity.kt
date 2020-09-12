6f68aaea136537befbc69c2aa3a11976423b3cac
mikail <mikailkyusuf@gmail.com>
2020-09-12 07:06:22 +0100

    Feature for user to logout

--- app/src/main/java/com/farmbuy/buyer/ui/BuyersActivity.kt
+++ app/src/main/java/com/farmbuy/buyer/ui/BuyersActivity.kt
@@ -1,12 +1,17 @@
 package com.farmbuy.buyer.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.viewpager2.widget.ViewPager2
 import com.farmbuy.R
 import com.farmbuy.adapters.ViewpagerAdapter
+import com.farmbuy.ui.LoginActivity
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
+import com.google.firebase.auth.FirebaseAuth
 
 class BuyersActivity : AppCompatActivity() {
 
@@ -37,5 +42,26 @@ class BuyersActivity : AppCompatActivity() {
                     1 -> tab.text = "My Orders"
                 }
             }).attach()
+    }
+
+
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.logout -> {
+                FirebaseAuth.getInstance().signOut()
+                val it = Intent(this, LoginActivity::class.java)
+                startActivity(it)
+                finish()
+                return true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
--- app/src/main/java/com/farmbuy/farmer/FarmersActivity.kt
+++ app/src/main/java/com/farmbuy/farmer/FarmersActivity.kt
@@ -1,8 +1,13 @@
 package com.farmbuy.farmer
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import com.farmbuy.R
+import com.farmbuy.ui.LoginActivity
+import com.google.firebase.auth.FirebaseAuth
 
 class FarmersActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,5 +17,24 @@ class FarmersActivity : AppCompatActivity() {
         val toolbar: androidx.appcompat.widget.Toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
         supportActionBar?.setDisplayShowTitleEnabled(false)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.farmersmenu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.logout -> {
+                FirebaseAuth.getInstance().signOut()
+                val it = Intent(this, LoginActivity::class.java)
+                startActivity(it)
+                finish()
+                return true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
